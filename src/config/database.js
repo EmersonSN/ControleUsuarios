2e6782890740322b3d02bdc2182933eced02ed32
Emerson <emersonn956@gmail.com>
2022-02-22 22:34:08 -0300

    Adicionado objeto de configuração com infos do BD

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect : 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '1234',
+    database: 'nodesql',
+    define:{
+        timestamp: true,
+        underscored: true,
+    }
+}//objeto de configurações/parametros da base de dados
